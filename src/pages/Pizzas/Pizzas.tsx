2c51136d79f1b47ef2e688f03dd4575d0d6c8cf2
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-05-27 22:50:03 +0100

    Add basic styles for the Pizzas page

--- src/pages/Pizzas/Pizzas.tsx
+++ src/pages/Pizzas/Pizzas.tsx
@@ -1,5 +1,16 @@
 import styles from './Pizzas.module.css';
 
 export default function Pizzas() {
-  return <section className={styles.pizzas}>Pizzas</section>;
+  return (
+    <section className={styles.pizzas}>
+      <h2 className={styles.pizzasTitle}>Pizzas</h2>
+      <div className={styles.pizzasDescription}>
+        <p>Our pizzas are prepared extremely fast, without compromising their quality!</p>
+        <p>
+          Did you know that our chef is the world champion in pizza acrobatics? He holds the Guinness World Record for
+          the fastest spinning of pizza dough!
+        </p>
+      </div>
+    </section>
+  );
 }

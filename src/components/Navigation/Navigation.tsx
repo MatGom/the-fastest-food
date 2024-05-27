b7f59ba81772fa421e9571685dd9dedbccc4527c
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-05-27 16:50:00 +0100

    Add basic styles to the Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,29 +1,33 @@
+import styles from './Navigation.module.css';
+
 import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    <nav>
-      <div>
+    <nav className={styles.navigation}>
+      <div className={styles.navigationTitle}>
         <h1>The Fastest Food</h1>
-        <div>Basket</div>
+        <div className={styles.navigationBasket}>
+          <i className='fa-solid fa-basket-shopping'></i>
+        </div>
       </div>
-      <ul>
-        <li>
+      <ul className={styles.navigationList}>
+        <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
           <Link to='/'>Home</Link>
         </li>
-        <li>
+        <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
           <Link to='/deals'>Deals</Link>
         </li>
-        <li>
+        <li className={styles.navigationListItem}>
           <Link to='/pizzas'>Pizzas</Link>
         </li>
-        <li>
+        <li className={styles.navigationListItem}>
           <Link to='/burgers'>Burgers</Link>
         </li>
-        <li>
+        <li className={styles.navigationListItem}>
           <Link to='/desserts'>Desserts</Link>
         </li>
-        <li>
+        <li className={styles.navigationListItem}>
           <Link to='/drinks'>Drinks</Link>
         </li>
       </ul>

1d1e0c76ca8f3d4423795814b5d3efff0cb78f1f
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-05-27 17:23:55 +0100

    Add media queries to the Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -11,26 +11,28 @@ export default function Navigation() {
           <i className='fa-solid fa-basket-shopping'></i>
         </div>
       </div>
-      <ul className={styles.navigationList}>
-        <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
-          <Link to='/deals'>Deals</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link to='/pizzas'>Pizzas</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link to='/burgers'>Burgers</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link to='/desserts'>Desserts</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link to='/drinks'>Drinks</Link>
-        </li>
-      </ul>
+      <div className={styles.navigationListContainer}>
+        <ul className={styles.navigationList}>
+          <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className={`${styles.navigationListItem} ${styles.navigationListMainItem}`}>
+            <Link to='/deals'>Deals</Link>
+          </li>
+          <li className={styles.navigationListItem}>
+            <Link to='/pizzas'>Pizzas</Link>
+          </li>
+          <li className={styles.navigationListItem}>
+            <Link to='/burgers'>Burgers</Link>
+          </li>
+          <li className={styles.navigationListItem}>
+            <Link to='/desserts'>Desserts</Link>
+          </li>
+          <li className={styles.navigationListItem}>
+            <Link to='/drinks'>Drinks</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
